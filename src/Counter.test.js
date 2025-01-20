cd1b2e3d5c07d2f0a0efad2c3d7f4350679f6d66
INFORMATICA\antonio.garcia <antoniogm56@educastur.es>
2025-01-20 16:42:59 +0100

    first commit

---
+++ src/Counter.test.js
@@ -0,0 +1,21 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import Counter from './Counter';
+
+test('Check first click', () => {
+    render (<Counter />);
+    fireEvent.click(screen.getByText(/Incrementar/i))
+    const contador2 = screen.getByText(/Contador: 1/i)
+    expect (contador2).toBeInTheDocument();
+});
+
+test('renders increment button', () => {
+    render (<Counter />);
+    const buttonElement = screen.getByText(/Incrementar/i)
+    expect (buttonElement).toBeInTheDocument();
+});
+
+test('Check initial value is 0', () => {
+    render (<Counter />);
+    const inicial = screen.getByText(/Contador: 0/i)
+    expect (inicial).toBeInTheDocument();
+});
